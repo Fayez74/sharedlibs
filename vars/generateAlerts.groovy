2fae2e0cb15b2e86bebfdc69bb2245ef6f098073
Fayez Ahmed <fayez.ahmed@hotmail.co.uk>
2021-09-21 17:21:17 +0100

    Update generateAlerts.groovy

--- vars/generateAlerts.groovy
+++ vars/generateAlerts.groovy
@@ -1,12 +1,16 @@
-def call(buildStatus,buildJobUrl){
-  pipeline {
+def call() {
+    pipeline {
     agent any
+
     stages {
-        stage('Generating Email') {
-          steps {
-              emailext body: 'Job ${buildJobUrl} was a ${buildStatus} !', subject: 'JOB BUILD: ${buildStatus}', to: 'fayez.aqtas@ecs.co.uk'
-                
+        stage('Git Checkout') {
+            steps {
+                emailext body: 'Job ${buildJobUrl} was a ${buildStatus} !', subject: 'JOB BUILD: ${buildStatus}', to: 'fayez.aqtas@ecs.co.uk'
             }
         }
+
+
+    }
 }
+
 }
