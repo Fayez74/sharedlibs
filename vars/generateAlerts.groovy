03127fd3405693c2aa8a6c63809513a8b3710c45
Fayez Ahmed <fayez.ahmed@hotmail.co.uk>
2021-09-22 10:59:22 +0100

    Update generateAlerts.groovy

--- vars/generateAlerts.groovy
+++ vars/generateAlerts.groovy
@@ -1,16 +1,37 @@
-def call() {
-    pipeline {
-    agent any
+#!/usr/bin/env groovy
 
-    stages {
-        stage('Git Checkout') {
-            steps {
-                emailext body: 'Job ${buildJobUrl} was a ${buildStatus} !', subject: 'JOB BUILD: ${buildStatus}', to: 'fayez.aqtas@ecs.co.uk'
-            }
-        }
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
 
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]': ${buildStatus}"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
-    }
-}
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
 
+  // Send notifications
+
+  emailext (
+      to: 'fayez.aqtas@ecs.co.uk',
+      subject: subject,
+      body: details
+    )
 }
